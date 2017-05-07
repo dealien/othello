7d6cd377ab21530eda11367e1d3fcf37a2282073
dealien <dragonofawsome@gmail.com>
2017-05-07 14:07:52 -0400

    added proper CSS for images

--- SCRIPT.js
+++ SCRIPT.js
@@ -3,26 +3,21 @@ Code drafted on CodePen:
 
 https://codepen.io/Vyren/pen/BRdVeM?editors=1010
 */
-
 console.log("");
 console.log("");
 console.log("");
-
 document.body.onload = start;
-
 var assets = [];
 var exists;
 var project = "othello";
 var path_to_images = "assets/";
 // var path_to_images = "images/buttons/";
-
 console.log("assets:", assets);
 
 function start() {
     testjQuery();
     // indexAssets();
 }
-
 $.when($.ajax(indexAssets())).then(function() {
     addImages();
 });
@@ -34,7 +29,6 @@ function testjQuery() {
         console.info("jQuery is loaded");
     }
 }
-
 // The "callback" argument is called with either true or false
 // depending on whether the image at "url" exists or not.
 function imageExists(url, callback) {
@@ -53,11 +47,7 @@ function indexAssets() {
     $("b:not([class])").each(function() {
         var assetName = $.trim($(this).text());
         console.log(assetName);
-        var path =
-            'https://dealien.gitbooks.io/' + project + '/content/' +
-            path_to_images +
-            assetName +
-            '.png';
+        var path = 'https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png';
         console.log(path);
         console.count("items tested for index");
         imageExists(path, function(exists) {
@@ -78,22 +68,12 @@ function addImages() {
         if (assets.indexOf(assetName) != -1) {
             exists = true;
             console.log("Image exists for", assetName);
-            return (
-                '<img src="https://dealien.gitbooks.io/' + project + '/content/' +
-                path_to_images +
-                assetName +
-                '.png" id="' +
-                assetName +
-                ' character-image"><b class="have-asset">' + assetName + '</b>');
+            return ('<img src="https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png" id="' + assetName + ' character-image"><b class="have-asset">' + assetName + '</b>');
         } else {
             exists = false;
             console.error("Image does not exist for", assetName);
             return '<b class="missing-asset">' + assetName + '</b>';
         }
-        console.log('https://dealien.gitbooks.io/' + project + '/content/' +
-            path_to_images +
-            assetName +
-            '.png'
-        );
+        console.log('https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png');
     });
 }
