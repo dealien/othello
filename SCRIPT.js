d3330e3c72acdb288248c4ccd16aabd58eefa07f
dealien <dragonofawsome@gmail.com>
2017-05-08 15:58:35 -0400

    optimized script
    - commented all console outputs
    - removed functions:
    	testjQuery()
    	start()

--- SCRIPT.js
+++ SCRIPT.js
@@ -4,11 +4,7 @@ Code drafted on CodePen:
 https://codepen.io/Vyren/pen/BRdVeM?editors=1010
 */
 
-console.log("");
-console.log("");
-console.log("");
-
-document.body.onload = start;
+// console.log("");console.log("");console.log("");
 
 var assets = [];
 var needed = [];
@@ -21,19 +17,6 @@ var path_to_images = "assets/";
 $.when($.ajax(indexAssets())).then(function() {
     addImages();
 });
-
-function start() {
-    testjQuery();
-    indexAssets();
-}
-
-function testjQuery() {
-    if (!window.jQuery) {
-        console.error("jQuery is not loaded");
-    } else {
-        console.info("jQuery is loaded");
-    }
-}
 
 // The "callback" argument is called with either true or false
 // depending on whether the image at "url" exists or not.
@@ -49,24 +32,24 @@ function imageExists(url, callback) {
 }
 
 function indexAssets() {
-    console.info("Running function indexAssets()");
+    // console.info("Running function indexAssets()");
     $("b:not([class])").each(function() {
         var assetName = $.trim($(this).text());
-        console.log(assetName);
+        // console.log(assetName);
         var path = 'https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png';
-        console.log(path);
-        console.count("items tested for index");
+        // console.log(path);
+        // console.count("items tested for index");
         imageExists(path, function(exists) {
-            console.log("RESULT: url=" + path + ", exists = " + exists);
+            // console.log("RESULT: url=" + path + ", exists = " + exists);
             if (exists === true) {
                 if (assets.indexOf(assetName) == -1) {
                     assets.push(assetName);
                 }
-                console.log("assets:", assets);
+                // console.log("assets:", assets);
             } else if (exists === false) {
                 if (needed.indexOf(assetName) == -1) {
                     needed.push(assetName);
-                    console.log("needed:", needed);
+                    // console.log("needed:", needed);
                 }
             }
         });
@@ -74,19 +57,19 @@ function indexAssets() {
 }
 
 function addImages() {
-    console.info("Running function addImages()");
+    // console.info("Running function addImages()");
     $("b:not([class])").replaceWith(function() {
         var assetName = $.trim($(this).text());
-        console.log(assetName);
+        // console.log(assetName);
         if (assets.indexOf(assetName) != -1) {
             exists = true;
-            console.log("Image exists for", assetName);
+            // console.log("Image exists for", assetName);
             return ('<a href="https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png class="image-url"><img src="https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png" id="' + assetName + '" class="character-image"></a><b class="have-asset">' + assetName + '</b>');
         } else {
             exists = false;
-            console.error("Image does not exist for", assetName);
+            // console.error("Image does not exist for", assetName);
             return '<b class="missing-asset">' + assetName + '</b>';
         }
-        console.log('https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png');
+        // console.log('https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png');
     });
 }
