6fd52f3794f427fc973837a537221a6663106426
dealien <dragonofawsome@gmail.com>
2017-06-02 12:02:30 -0400

    auto-update via bash

--- scripts/SCRIPT.js
+++ scripts/SCRIPT.js
@@ -66,11 +66,11 @@ function addImages() {
         if (assets.indexOf(assetName) != -1) {
             exists = true;
             // console.log("Image exists for", assetName);
-            return ('<a href="https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png" class="image-url"><img src="https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png" id="' + assetName + '" class="character-image"></a><b class="has-asset" onclick="indexAssets()">' + assetName + '</b>');
+            return ('<a href="https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png" class="image-url"><img src="https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png" id="' + assetName + '" class="character-image"></a><b class="has-asset" onclick="reacquireAssets()">' + assetName + '</b>');
         } else {
             exists = false;
             // console.error("Image does not exist for", assetName);
-            return '<b id="missing-asset" onclick="indexAssets()">' + assetName + '</b>';
+            return '<b id="missing-asset" onclick="reacquireAssets()">' + assetName + '</b>';
         }
         // console.log('https://dealien.gitbooks.io/' + project + '/content/' + path_to_images + assetName + '.png');
     });
@@ -92,10 +92,10 @@ function copyRef(line) {
 }
 
 // Run indexAssets() when clicking on a character's name
-$("b").click(function() {
+function reacquireAssets() {
     var assets = [];
     var needed = [];
     console.info("object clicked", this);
     indexAssets();
     addImages();
-});
+}
